ad6e2eb1ed80ca44de08b991c6ae19125ccccdca
aripoya <wahwooh@gmail.com>
2012-12-22 18:27:36 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siloam::Application.config.secret_token = '5625a0b743a2b5cd669cd2a257bf69e920a9578c61981287e7759636bd69bb23cf60e29ec89de8608df858bd4d5376ea6b89bb1a90f912e6a3322bcba9783a29'
